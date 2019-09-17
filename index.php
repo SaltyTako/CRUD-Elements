66223ae9414f89d43af232d81192f6601d9fe627
Rafael <takeshie01@gmail.com>
2019-09-16 21:30:04 -0430

    Login frontend modified

--- index.php
+++ index.php
@@ -1,16 +1,45 @@
 <?php include('includes/header.php'); ?>
-    <form action="sv.php" method="post">
-    <input type="text" name="Username" placeholder="Username or Email">
-    <input type="password" name="Password" placeholder="Password">
-    <input type="submit" name="Register">
-    </form>
-    <div>
-    <form action="verifier.php" method="post">
-    <input type="text" name="Username" placeholder="Username or Email">
-    <input type="password" name="Password" placeholder="Password">
-    <input type="submit" name="Login">
-    </form>
+
+    <div class="container-fluid text-center">
+        
+        <div class="row">
+            <div class="offset-md-5 row-md-4">
+                <h2>Registrarse</h2>
+                <hr>    
+                    <!--Register-->
+                    <form action="sv.php" method="post">
+                        <div class="form-group">
+                            <input type="text" name="Username" placeholder="Nombre de usuario"><br>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="Password" placeholder="Contraseña"><br>
+                        </div>
+                        <div class="form-group">
+                            <input class="btn btn-primary" type="submit" name="Register" value="Registrarse"><br>    
+                        </div>
+                    </form>
+
+                <h2>Iniciar sesion</h2>
+                <hr>  
+                    <!--Login-->
+                    <form action="verifier.php" method="post">
+                        <div class="form-group">
+                            <input type="text" name="Username" placeholder="Nombre de usuario">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="Password" placeholder="Contraseña">
+                        </div>
+                        <div class="form-group">
+                            <input class="btn btn-primary" type="submit" name="Login" value="Entrar">
+                        </div>
+                    </form>
+                    
+            </div>
+        </div>
+        
+        
     </div>
+    
     
     <?php
     //TODO list:
